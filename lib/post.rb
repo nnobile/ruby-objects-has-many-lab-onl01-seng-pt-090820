9f12d82fcade1dc03739cc021d4f778c79f24ed8
Nick Nobile <nickcnobile@gmail.com>
2020-10-05 03:42:59 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -7,6 +7,14 @@ class Post
     @@all << self
     end
 
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+
     def self.all
       @@all
     end
