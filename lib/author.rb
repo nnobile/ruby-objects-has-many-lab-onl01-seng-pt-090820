517a5182abd6ce32a0600f3c6dbdc9870ee8f61d
Nick Nobile <nickcnobile@gmail.com>
2020-10-05 03:30:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,8 +17,8 @@ end
 
 def add_post_by_title(post_title)
   Post.new(post_title)
-  author = Post.new(post_title)
-  add_post(author)
+  writer = Post.new(post_title)
+  add_post(writer)
 end
 
 end
