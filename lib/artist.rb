ae4844aed08b97a5e1ab0eedefdc22f445847b2c
Nick Nobile <nickcnobile@gmail.com>
2020-10-05 01:13:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,17 @@
 class Artist
-  attr_accessor :name, :songs, :genre
+  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
 
   def add_song(song)
-    @songs << song
     song.artist = self
   end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 end
 
 beyonce = Artist.new("Beyonce")
