5fb29e1f447adf988977630259d2fd73521a636a
Nick Nobile <nickcnobile@gmail.com>
2020-10-05 03:19:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,14 @@
 class Author
-  attr_accessor :name, :post
+  attr_accessor :name, :posts
 
   def initialize (name)
     @name = name
   end
-  
+
+  def posts
+    Post.all.select do |post|
+      post.author == self
+  end
+end
+
 end
